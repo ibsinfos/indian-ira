2c2229c973cab363ebdc8846741720de62dbcf46
MehulBawadia <mehulbawadia@gmail.com>
2018-06-06 12:36:39 +0530

    Installed and configured Laravel

---
+++ README.md
@@ -0,0 +1,15 @@
+# Indian Ira
+
+The complete source code of the project
+
+### Installation
+
+Step 1: Clone the repository
+```
+$ git clone git@github.com:detatech/indian-ira.git
+```
+
+Step 2. Install Node Modules
+```
+npm install && npm run dev
+```
---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace IndianIra\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'IndianIra\Events\Event' => [
+            'IndianIra\Listeners\EventListener',
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}
