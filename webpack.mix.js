338d66acd8009ecc3aadaf8a1583aa520b22dc5c
MehulBawadia <mehulbawadia@gmail.com>
2018-06-07 12:43:10 +0530

    Integrated iGrowl Notificaton
    Plugin link: http://catc.github.io/iGrowl/

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,22 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+// mix.js('resources/assets/js/app.js', 'public/js')
+//    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.js([
+    'resources/assets/js/app.js',
     'resources/assets/js/character-maxlength.js',
+    'resources/assets/libs/iGrowl/javascripts/igrowl.js'
+], 'public/js/app.js');
+
+mix.styles([
+    'public/css/app.css',
+    'resources/assets/libs/iGrowl/stylesheets/animate.css',
+    'resources/assets/libs/iGrowl/stylesheets/igrowl.css',
+    'resources/assets/libs/iGrowl/stylesheets/icomoon/feather.css',
+], 'public/css/app.css');
+
+mix.copyDirectory('resources/assets/libs/iGrowl/fonts/feather', 'public/fonts/feather');
