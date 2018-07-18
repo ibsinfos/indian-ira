a5f96f5f4757a26dda216437df4f18548c305d7c
MehulBawadia <mehulbawadia@gmail.com>
2018-07-18 12:14:03 +0530

    Added categories and shipping rates in seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 use IndianIra\Product;
 use IndianIra\Carousel;
+use IndianIra\Category;
+use IndianIra\ShippingRate;
 use Illuminate\Database\Seeder;
 use IndianIra\ProductPriceAndOption;
 
@@ -24,8 +26,19 @@ class DatabaseSeeder extends Seeder
 
         factory(ProductPriceAndOption::class, 20)->create(['image' => null]);
         factory(Carousel::class, 2)->create(['display' => 'Enabled']);
+        factory(Category::class, 2)->create(['display' => 'Enabled']);
 
         $allProducts = Product::whereDisplay('Enabled')->get();
+
+        $allProducts->take(10)->shuffle()->each(function ($product) {
+            $product->categories()->attach([Category::first()->id]);
+        });
+        $allProducts->whereNotIn('id', Category::first()->products->pluck('id')->toArray())
+                    ->take(10)
+                    ->shuffle()
+                    ->each(function ($product) {
+                        $product->categories()->attach([Category::all()->last()->id]);
+                    });
 
         $lastCarousel = Carousel::all()->last();
         $lastCarousel->products()->attach($allProducts->take(6)->pluck('id')->shuffle()->toArray());
@@ -39,5 +52,18 @@ class DatabaseSeeder extends Seeder
                                 ->get();
         $carousel = Carousel::all()->last();
         $carousel->products()->attach($allProducts->pluck('id')->shuffle()->toArray());
+
+        $ratesAndWeight = collect();
+        $ratesAndWeight->push(['from' => 1, 'to' => 1000, 'amount' => 50]);
+        $ratesAndWeight->push(['from' => 1000, 'to' => 1500, 'amount' => 100]);
+        $ratesAndWeight->push(['from' => 1500, 'to' => 2000, 'amount' => 150]);
+
+        $ratesAndWeight->each(function ($row) {
+            factory(ShippingRate::class)->create([
+                'weight_from'   => $row['from'],
+                'weight_to'     => $row['to'],
+                'amount' => $row['amount'],
+            ]);
+        });
     }
 }
